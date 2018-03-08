7f19df192dbb88cdb1ba20ccef3ec6864844eb41
doganekici <doganekici26@gmail.com>
2018-03-08 13:58:40 +0300

    Added iOS

---
+++ firebase-iOS.js
@@ -0,0 +1,61 @@
+const FirebaseAnalytics = require("./firebaseAnalytics");
+const FirebaseApp = require("./firebaseApp");
+const FirebaseAuth = require("./firebaseAuth");
+const FirebaseMessaging = require("./firebaseMessaging");
+
+function Firebase() {};
+
+Firebase.initializeApp = function(options, name) {
+
+    if (options && options.iosFile.exists) {
+        
+        var pathPlist = options.iosFile.nativeObject.getActualPath();
+
+        var alloc = Invocation.invokeClassMethod("FIROptions","alloc",[],"id");
+        var argPathPlist = new Invocation.Argument({
+            type:"NSString",
+            value: pathPlist
+        });
+        var firOptions = Invocation.invokeInstanceMethod(alloc,"initWithContentsOfFile:",[argPathPlist],"NSObject");
+
+        var nativeFirebaseApp;
+        if(name){
+            FirebaseApp.ios.native.configureWithNameOptions(name,firOptions);
+            nativeFirebaseApp = FirebaseApp.ios.native.appNamed(name);
+        }
+        else {
+            FirebaseApp.ios.native.configureWithOptions(firOptions);
+            nativeFirebaseApp = FirebaseApp.ios.native.defaultApp();
+        }
+        return new FirebaseApp(nativeFirebaseApp);
+
+    }
+};
+
+Firebase.app = function(name) {
+    var nativeFirebaseApp;
+    if(name){
+        nativeFirebaseApp = FirebaseApp.ios.native.appNamed(name);
+    }else {
+        nativeFirebaseApp = FirebaseApp.ios.native.defaultApp();
+    }
+    return FirebaseApp(nativeFirebaseApp);
+};
+
+Firebase.apps = function() {
+    return FirebaseApp.ios.native.allApps();
+};
+
+Firebase.auth = function(FirebaseApp) {
+    return new FirebaseAuth(FirebaseApp);
+};
+
+Firebase.analytics = function(FirebaseApp) {
+    return new FirebaseAnalytics();
+};
+
+Firebase.messaging = function(FirebaseApp) {
+    return new FirebaseMessaging();
+};
+
+module.exports = Firebase;
