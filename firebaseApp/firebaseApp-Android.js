4d43d005f1f0fab22e9dfd323b6f360221b8b8ef
“Metehan <metehan.semiz@smartface.io>
2018-03-09 06:20:27 +0000

    added js

--- firebaseApp/firebaseApp-Android.js
+++ firebaseApp/firebaseApp-Android.js
@@ -1,56 +1,71 @@
 const FirebaseAuth = require("../firebaseAuth");
+const AndroidConfig = require("sf-core/util/Android/androidconfig");
 
 function FirebaseApp(nativeFirebaseApp) {
     var self = this;
-
+    
     this.nativeObject = nativeFirebaseApp;
 
     Object.defineProperties(self, {
         'auth': {
             value: function() {
-                return new FirebaseAuth(self);
+                if (!AndroidConfig.isEmulator) {
+                    return new FirebaseAuth(self);
+                }
             },
             enumerable: true,
             configurable: true
         },
         'getName': {
             value: function() {
-                return self.nativeObject.getName();
+                if (!AndroidConfig.isEmulator) {
+                    return self.nativeObject.getName();
+                }
             },
             enumerable: true,
             configurable: true
         },
         'getApiKey': {
             value: function() {
-                return self.nativeObject.getOptions().getApiKey();
+                if (!AndroidConfig.isEmulator) {
+                    return self.nativeObject.getOptions().getApiKey();
+                }
             },
             enumerable: true,
             configurable: true
         },
         'getApplicationId': {
             value: function() {
-                return self.nativeObject.getOptions().getApplicationId();
+                if (!AndroidConfig.isEmulator) {
+                    return self.nativeObject.getOptions().getApplicationId();
+                }
             },
             enumerable: true,
             configurable: true
         },
         'getDatabaseUrl': {
             value: function() {
-                return self.nativeObject.getOptions().getDatabaseUrl();
+                if (!AndroidConfig.isEmulator) {
+                    return self.nativeObject.getOptions().getDatabaseUrl();
+                }
             },
             enumerable: true,
             configurable: true
         },
         'getGcmSenderId': {
             value: function() {
-                return self.nativeObject.getOptions().getGcmSenderId();
+                if (!AndroidConfig.isEmulator) {
+                    return self.nativeObject.getOptions().getGcmSenderId();
+                }
             },
             enumerable: true,
             configurable: true
         },
         'getStorageBucket': {
             value: function() {
-                return self.nativeObject.getOptions().getStorageBucket();
+                if (!AndroidConfig.isEmulator) {
+                    return self.nativeObject.getOptions().getStorageBucket();
+                }
             },
             enumerable: true,
             configurable: true
