a0f56400f6189f1cbc671daaaa42cc9599860c40
Dogan Ekici <doganekici26@gmail.com>
2018-03-15 12:34:20 +0000

    Update firebase ios

--- firebaseAuth/firebaseAuth-iOS.js
+++ firebaseAuth/firebaseAuth-iOS.js
@@ -1,3 +1,205 @@
-function FirebaseAuth() {}
+const Invocation = require('sf-core/util').Invocation;
+
+function FirebaseAuth(FirebaseApp) {
+	var self = this;
+
+	if (FirebaseApp) {
+        self.nativeAuth = FirebaseAuth.ios.native.authWithApp(FirebaseApp);
+	}else{
+        self.nativeAuth = FirebaseAuth.ios.native.auth();
+    }
+
+    Object.defineProperties(self, {
+        'languageCode': {
+            get: function() {
+                return FirebaseAuth.ios.native.getLanguageCode(self.nativeAuth);
+            },
+            set: function(value) {
+                FirebaseAuth.ios.native.setLanguageCode(self.nativeAuth,value);
+            },
+            enumerable: true,
+            configurable: true
+        },
+        'getCurrentUser': {
+            value: function() {
+                //FBUser
+                return FirebaseAuth.ios.native.currentUser(self.nativeAuth);
+            },
+            enumerable: true,
+            configurable: true
+        },
+        'createUserWithEmailAndPassword': {
+            value: function(email, password, callback) {
+                var callbackHandler = function(e){
+                    if (typeof callback === 'function') {
+                        if (e.error) {
+                            callback(undefined,e.error);
+                        }else{
+                            //FBUser
+                            callback(e.user,undefined);
+                        }
+                    }
+                };
+                FirebaseAuth.ios.native.createUserWithEmailPasswordCompletion(self.nativeAuth,email,password,callbackHandler);
+            },
+            enumerable: true,
+            configurable: true
+        },
+        'signInWithEmailAndPassword': {
+            value: function(email, password, callback) {
+                var callbackHandler = function(e){
+                    if (typeof callback === 'function') {
+                        if (e.error) {
+                            callback(undefined,FirebaseAuth.ios.native.getErrorObject(e.error));
+                        }else{
+                            //FBUser
+                            callback(e.user,undefined);
+                        }
+                    }
+                };
+                FirebaseAuth.ios.native.signInWithEmailPasswordCompletion(self.nativeAuth,email,password,callbackHandler);
+            },
+            enumerable: true,
+            configurable: true
+        },
+        'signInWithCustomToken': {
+            value: function(token, callback) {
+
+            },
+            enumerable: true,
+            configurable: true
+        },
+        'signInAnonymously': {
+            value: function(callback) {
+                
+            },
+            enumerable: true,
+            configurable: true
+        },
+        'signOut': {
+            value: function() {
+   
+            },
+            enumerable: true,
+            configurable: true
+        },
+    });
+}
+
+FirebaseAuth.ios = {};
+FirebaseAuth.ios.native = {};
+
+FirebaseAuth.ios.native.auth = function(){
+	return Invocation.invokeClassMethod("FIRAuth","auth",[],"NSObject");
+};
+
+FirebaseAuth.ios.native.authWithApp = function(app){
+    var argApp = new Invocation.Argument({
+        type:"NSObject",
+        value: app
+    });
+	return Invocation.invokeClassMethod("FIRAuth","authWithApp:",[argApp],"NSObject");
+};
+
+FirebaseAuth.ios.native.app = function(auth){
+	return Invocation.invokeInstanceMethod(auth,"app",[],"NSObject");
+}
+
+FirebaseAuth.ios.native.currentUser = function(auth){
+	return Invocation.invokeInstanceMethod(auth,"currentUser",[],"NSObject");
+}
+
+FirebaseAuth.ios.native.getLanguageCode = function(auth){
+	return Invocation.invokeInstanceMethod(auth,"languageCode",[],"NSString");
+}
+
+FirebaseAuth.ios.native.setLanguageCode = function(auth,languageCode){
+    var argLanguageCode = new Invocation.Argument({
+        type:"NSString",
+        value: languageCode
+    });
+    Invocation.invokeInstanceMethod(auth,"setLanguageCode",[argLanguageCode]);
+}
+///////Error Codes
+FirebaseAuth.ios.native.signInWithEmailPasswordCompletion = function(auth,email,password,completion)
+{
+	var argEmail = new Invocation.Argument({
+        type:"NSString",
+        value: email
+    });
+    var argPassword = new Invocation.Argument({
+        type:"NSString",
+        value: password
+    });
+    var argCompletion = new Invocation.Argument({
+        type:"FIRAuthResultCallback",
+        value: function(e){
+        	completion(e);
+        }
+    });
+	Invocation.invokeInstanceMethod(auth,"signInWithEmail:password:completion:",[argEmail,argPassword,argCompletion]);
+}
+///////Error Codes
+FirebaseAuth.ios.native.createUserWithEmailPasswordCompletion = function(auth,email,password,completion)
+{
+    var argEmail = new Invocation.Argument({
+        type:"NSString",
+        value: email
+    });
+    var argPassword = new Invocation.Argument({
+        type:"NSString",
+        value: password
+    });
+    var argCompletion = new Invocation.Argument({
+        type:"FIRAuthResultCallback",
+        value: function(e){
+            completion(e);
+        }
+    });
+    Invocation.invokeInstanceMethod(auth,"createUserWithEmail:password:completion:",[argEmail,argPassword,argCompletion]);
+}
+///////Error Codes
+FirebaseAuth.ios.native.signInWithCustomTokenCompletion = function(auth,token,completion)
+{
+    var argToken = new Invocation.Argument({
+        type:"NSString",
+        value: token
+    });
+    var argCompletion = new Invocation.Argument({
+        type:"FIRAuthResultCallback",
+        value: function(e){
+            completion(e);
+        }
+    });
+    Invocation.invokeInstanceMethod(auth,"signInWithCustomToken:completion:",[argToken,argCompletion]);
+}
+///////Error Codes
+FirebaseAuth.ios.native.signInAnonymouslyWithCompletion = function(auth,completion)
+{
+    var argCompletion = new Invocation.Argument({
+        type:"FIRAuthResultCallback",
+        value: function(e){
+            completion(e);
+        }
+    });
+    Invocation.invokeInstanceMethod(auth,"signInAnonymouslyWithCompletion:",[argCompletion]);
+}
+///////Error Codes
+FirebaseAuth.ios.native.signOut = function(auth)
+{
+    var argToken = new Invocation.Argument({
+        type:"NSObject",
+        value: undefined
+    });
+    return Invocation.invokeInstanceMethod(auth,"signOut:",[]);
+}
+
+FirebaseAuth.ios.native.getErrorObject = function(nativeError)
+{
+    var code = Invocation.invokeInstanceMethod(nativeError,"code",[],"NSInteger");
+    var localizedDescription = Invocation.invokeInstanceMethod(nativeError,"localizedDescription",[],"NSString");
+
+    return {code: code, description: localizedDescription};
+}
 
 module.exports = FirebaseAuth;
--- firebaseMessaging/firebaseMessaging-iOS.js
+++ firebaseMessaging/firebaseMessaging-iOS.js
@@ -1,3 +1,73 @@
+const Invocation = require('sf-core/util').Invocation;
+
 function FirebaseMessaging() {}
+
+FirebaseMessaging.ios = {};
+
+FirebaseMessaging.subscribeToTopic = function(topic){
+	FirebaseMessaging.ios.native.subscribeToTopic(topic);
+}
+
+FirebaseMessaging.unsubscribeFromTopic = function(topic){
+	FirebaseMessaging.ios.native.unsubscribeFromTopic(topic);
+}
+
+FirebaseMessaging.getToken = function(callback){
+	if (typeof callback === 'function'){
+		callback(FirebaseMessaging.ios.native.FCMToken());
+	}
+}
+
+FirebaseMessaging.ios.onTokenReflesh = function(){}
+
+// //////////// Native //////////////
+FirebaseMessaging.ios.native = {};
+
+FirebaseMessaging.ios.native.messaging = function(){
+	return Invocation.invokeClassMethod("FIRMessaging","messaging",[],"NSObject");
+}
+
+FirebaseMessaging.ios.native.FCMToken = function(){
+	return Invocation.invokeInstanceMethod(FirebaseMessaging.ios.native.messaging(),"FCMToken",[],"NSString");
+}
+
+FirebaseMessaging.ios.native.subscribeToTopic = function(topic){
+    var argTopic = new Invocation.Argument({
+        type:"NSString",
+        value: topic
+    });
+    Invocation.invokeInstanceMethod(FirebaseMessaging.ios.native.messaging(),"subscribeToTopic:",[argTopic]);
+}
+
+FirebaseMessaging.ios.native.unsubscribeFromTopic = function(topic){
+    var argTopic = new Invocation.Argument({
+        type:"NSString",
+        value: topic
+    });
+    Invocation.invokeInstanceMethod(FirebaseMessaging.ios.native.messaging(),"unsubscribeFromTopic:",[argTopic]);
+}
+
+FirebaseMessaging.ios.native.messagingDidReceiveRegistrationToken = function(){
+	if (!FirebaseMessaging.ios.native.messagingDelegate) {
+		var messagingAlloc = Invocation.invokeClassMethod("MessagingDelegate","alloc",[],"id");
+		FirebaseMessaging.ios.native.messagingDelegate = Invocation.invokeInstanceMethod(messagingAlloc,"init",[],"NSObject");
+		var argDelegate = new Invocation.Argument({
+        	type: "NSObject",
+        	value: FirebaseMessaging.ios.native.messagingDelegate
+    	});       
+    	Invocation.invokeInstanceMethod(FirebaseMessaging.ios.native.messaging(),"setDelegate:",[argDelegate]);
+	}
+	var argMessaggingDidRecieve = new Invocation.Argument({
+	    type:"JSValue",
+	    value: function(e){
+	    	if (typeof FirebaseMessaging.ios.onTokenReflesh === 'function') {
+	    		FirebaseMessaging.ios.onTokenReflesh(e.fcmToken);
+	    	}
+	    }
+    });
+    Invocation.invokeInstanceMethod(FirebaseMessaging.ios.native.messagingDelegate,"setMessagingDidReceiveRegistrationToken:",[argMessaggingDidRecieve]);
+}
+
+FirebaseMessaging.ios.native.messagingDidReceiveRegistrationToken();
 
 module.exports = FirebaseMessaging;
