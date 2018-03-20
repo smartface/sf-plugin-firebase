c0a5650d9f9eeddb5995e8f73b2ea6efe974dc90
Dogan Ekici <doganekici26@gmail.com>
2018-03-20 11:37:33 +0000

    Added analysis

--- analysis.js
+++ analysis.js
@@ -1,21 +1,74 @@
 function Firebase() {}
 
-// name optional
-// options = {iOSGooglePlistPath:"", androidGoogleServiceJson:""}
-Firebase.initializeApp = function(options,name) {};
+/**
+ * Initialize your SDK
+ *
+ * @event initializeApp
+ * @param {Object} config
+ * @param {IO.File} config.iosFile
+ * @param {IO.File} config.androidFile
+ * @param {String} name(Optional)
+ * @android
+ * @ios
+ * @since 0.1
+ */
+Firebase.initializeApp = function(config,name) {};
 
-// Return the default app or named app -- name optional
-// Return firebaseApp
+/**
+ * When called with no arguments, the default app is returned. 
+ * When an app name is provided, the app corresponding to that name is returned.
+ *
+ * @event app
+ * @param {String} name(Optional)
+ * @return {FirebaseApp} app
+ * @android
+ * @ios
+ * @since 0.1
+ */
 Firebase.app = function(name) {};
 
-// Return firebaseApp array
+/**
+ * Gets the FirebaseApp Array.
+ * 
+ * @event apps
+ * @param {String} name(Optional)
+ * @return {Array} apps
+ * @android
+ * @ios
+ * @since 0.1
+ */
 Firebase.apps = function() {};
 
-// Return firebaseApp Auth
+/**
+ * Gets the Auth service for the default app or a given app.
+ * 
+ * @event auth
+ * @param {FirebaseApp} FirebaseApp(Optional)
+ * @return {FirebaseAuth}
+ * @android
+ * @ios
+ * @since 0.1
+ */
 Firebase.auth = function(FirebaseApp) {};
 
-// Return firebaseApp Analytic
+/**
+ * Gets the Analytics service.
+ * 
+ * @event analytics
+ * @return {FirebaseAnalytics}
+ * @android
+ * @ios
+ * @since 0.1
+ */
 Firebase.analytics = function() {};
 
-// Return firebaseApp messaging
+/**
+ * Gets the messaging service.
+ * 
+ * @event messaging
+ * @return {FirebaseMessaging}
+ * @android
+ * @ios
+ * @since 0.1
+ */
 Firebase.messaging = function() {};
