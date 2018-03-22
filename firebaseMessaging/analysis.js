9f32cb88dff4d5b18912ed7917e2b00b246d8408
Dogan Ekici <doganekici26@gmail.com>
2018-03-22 08:33:54 +0000

    Updated message analysis

--- firebaseMessaging/analysis.js
+++ firebaseMessaging/analysis.js
@@ -1,7 +1,53 @@
 function FirebaseMessaging() {}
 
-FirebaseMessaging.prototype.ios.onTokenReflesh = function() {}; // this is callback function
+/**
+ *  The FCM token is used to identify this device so that FCM can send notifications to it.
+ *  It is associated with your APNS token when the APNS token is supplied, so that sending
+ *  messages to the FCM token will be delivered over APNS.
+ *
+ *  The FCM token is sometimes refreshed automatically. `onTokenReflesh` method will be called once a token is
+ *  available, or has been refreshed. Typically it should be called once per app start, but
+ *  may be called more often, if token is invalidated or updated.
+ *
+ *  Once you have an FCM token, you should send it to your application server, so it can use
+ *  the FCM token to send notifications to your device.
+ *
+ * @event onTokenReflesh
+ * @ios
+ * @since 0.1
+ */
+FirebaseMessaging.prototype.ios.onTokenReflesh = function(fcmToken) {};
+
+/**
+ * iOS : Returns the FCM token.
+ * Android : Returns the notification token.
+ * 
+ * @event getToken
+ * @android
+ * @ios
+ * @since 0.1
+ */
 FirebaseMessaging.prototype.getToken = function() {}; // Return string
-FirebaseMessaging.prototype.subscribeToTopic = function(topic) {}; // Return string
-FirebaseMessaging.prototype.unsubscribeFromTopic = function(topic) {}; // Return string
+
+/**
+ * Asynchronously subscribes to a topic.
+ * 
+ * @event subscribeToTopic
+ * @param {String} topic
+ * @android
+ * @ios
+ * @since 0.1
+ */
+FirebaseMessaging.prototype.subscribeToTopic = function(topic) {};
+
+/**
+ * Asynchronously unsubscribe from a topic.
+ * 
+ * @event unsubscribeFromTopic
+ * @param {String} topic
+ * @android
+ * @ios
+ * @since 0.1
+ */
+FirebaseMessaging.prototype.unsubscribeFromTopic = function(topic) {};
 
