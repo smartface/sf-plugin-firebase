60af6c161ce1787facd5a1c13c76aa32d43fe1cb
“Metehan <metehan.semiz@smartface.io>
2018-03-30 09:48:00 +0300

    fixed

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,7 +3,7 @@ mv ./plugins/firebaseios.zip ~/workspace/plugins/iOS/firebaseios.zip
 mv ./plugins/firebaseandroid.zip ~/workspace/plugins/Android/firebaseandroid.zip
 rm -rf ./plugins
 
-mv ./config/Android/strings.xml ~/workspace/config/Android/strings.xml
+mv -R -u -p ./config/Android/strings.xml ~/workspace/config/Android/strings.xml
 rm -rf ./config
 
 
