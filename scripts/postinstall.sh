55cc833907045410eb7070b888ee9c1729a02e2b
doganekici <doganekici26@gmail.com>
2018-11-30 14:44:47 +0300

    changed postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-mv ./plugins/firebaseios.zip ~/workspace/plugins/iOS/firebaseios.zip
-mv ./plugins/firebaseandroid.zip ~/workspace/plugins/Android/firebaseandroid.zip
-rm -rf ./plugins
+wget https://github.com/smartface/sf-plugin-firebase/raw/master/plugins/firebaseandroid.zip -O ~/workspace/plugins/Android/firebaseandroid.zip
+wget https://github.com/smartface/sf-plugin-firebase/raw/master/plugins/firebaseios.zip -O ~/workspace/plugins/iOS/firebaseios.zip
 
 mv -u ./config/Android/strings.xml ~/workspace/config/Android/strings.xml
 rm -rf ./config
