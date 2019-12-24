939986b49e4335fffcf1ea17d87a13948e321f2f
doganekici <doganekici26@gmail.com>
2019-12-24 13:49:15 +0300

    changed absolute path to relative path on post scripts

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-wget https://github.com/smartface/sf-plugin-firebase/raw/master/plugins/firebaseios.zip -O ~/workspace/plugins/iOS/firebaseios.zip
+wget https://github.com/smartface/sf-plugin-firebase/raw/master/plugins/firebaseios.zip -O $parent_path/../../../../plugins/iOS/firebaseios.zip
 
 rm -rf ./config
 
-mv $parent_path/../Native/Android/firebaseplugin ~/workspace/plugins/Android/firebaseplugin
+mv $parent_path/../Native/Android/firebaseplugin $parent_path/../../../../plugins/Android/firebaseplugin
 (
     cd "$parent_path"
     node ./project.js
