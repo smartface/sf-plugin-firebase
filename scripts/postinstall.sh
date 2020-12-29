9e2e3a0618439ed1a71fafe1544928dfda45348c
hasretsariyer <hasretsariyer@gmail.com>
2020-12-29 09:58:22 +0300

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-curl -o $parent_path/../../../../plugins/iOS/firebaseios.zip https://cd.smartface.io/repository/smartfacefirebase/ios/3.0.2/firebaseios.zip
+curl -o $parent_path/../../../../plugins/iOS/firebaseios.zip https://cd.smartface.io/repository/smartfacefirebase/ios/3.0.3/firebaseios.zip
 
 rm -rf ./config
 
