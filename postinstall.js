6760fae8da0d5e78511529510bdff672af4b8d50
Cihat Yeşildağ <cico643@hotmail.com.tr>
2021-12-03 11:31:13 +0300

    Check the existence of project.json in postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,73 +1,83 @@
-const fs = require("fs");
-const https = require("https");
-const path = require("path");
-const rimraf = require("./rimraf");
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const rimraf = require('./rimraf');
 
-const FIREBASE_URL = "https://cd.smartface.io/repository/smartfacefirebase/ios/3.0.4/firebaseios.zip";
+const FIREBASE_URL = 'https://cd.smartface.io/repository/smartfacefirebase/ios/3.0.4/firebaseios.zip';
+
+function checkExistenceOfProjectJson() {
+    const projectJSONPath = path.normalize(path.join(__dirname, '../../../../config/project.json'));
+    if (!fs.existsSync(projectJSONPath)) {
+        throw new Error(`To run postinstall properly ${projectJSONPath} is needed`);
+    }
+}
 
 async function getIOSFirebasePlugin() {
-  const projectPluginPath = path.normalize(path.join(__dirname, "../../../../plugins/iOS/firebaseios.zip"));
-  return new Promise((resolve, reject) => {
-    https.get(FIREBASE_URL, (res) => {
-      const filePath = fs.createWriteStream(projectPluginPath);
-      res.pipe(filePath);
-      filePath.on('finish', () => {
-          filePath.close();
-          resolve();
-      });
-      filePath.on('error', (err) => reject(err));
+    checkExistenceOfProjectJson();
+    const projectPluginPath = path.normalize(path.join(__dirname, '../../../../plugins/iOS/firebaseios.zip'));
+    return new Promise((resolve, reject) => {
+        https.get(FIREBASE_URL, (res) => {
+            const filePath = fs.createWriteStream(projectPluginPath);
+            res.pipe(filePath);
+            filePath.on('finish', () => {
+                filePath.close();
+                resolve();
+            });
+            filePath.on('error', (err) => reject(err));
+        });
     });
-  });
 }
 
 async function getAndroidFirebasePlugin() {
-  const androidFirebasePath = path.normalize(path.join(__dirname, "Native/Android/firebaseplugin"));
-  const projectPluginPath = path.normalize(path.join(__dirname, "../../../../plugins/Android/firebaseplugin"));
-  rimraf.sync(projectPluginPath, { recursive: true, force: true, disableGlob: true });
-  return new Promise((resolve, reject) => {
-    fs.rename(androidFirebasePath, projectPluginPath, (err) => {
-      if (err) {
-        reject(err);
-      }
-      resolve();
+    checkExistenceOfProjectJson();
+    const androidFirebasePath = path.normalize(path.join(__dirname, 'Native/Android/firebaseplugin'));
+    const projectPluginPath = path.normalize(path.join(__dirname, '../../../../plugins/Android/firebaseplugin'));
+    rimraf.sync(projectPluginPath, { recursive: true, force: true, disableGlob: true });
+    return new Promise((resolve, reject) => {
+        fs.rename(androidFirebasePath, projectPluginPath, (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
     });
-  });
 }
 
 function addDefaultConfigToProjectJSON() {
-  const projectJSONPath = path.normalize(path.join(__dirname,"../../../../config/project.json"));
-  const project = require(projectJSONPath);
-  if (!project.build.input.ios.plugins.firebaseios) {
-    project.build.input.ios.plugins.firebaseios = {
-      path: "plugins/iOS/firebaseios.zip",
-      active: true,
-      crashlytics: true
-    };
-  }
-  if (!project.build.input.android.plugins.modules || !project.build.input.android.plugins.modules.firebaseandroid) {
-      project.build.input.android.plugins.modules = project.build.input.android.plugins.modules ? project.build.input.android.plugins.modules : {};
-      project.build.input.android.plugins.modules.firebaseplugin = {
-          path: "plugins/Android/firebaseplugin",
-          active: true
-      };
-  }
+    const projectJSONPath = path.normalize(path.join(__dirname, '../../../../config/project.json'));
+    const project = require(projectJSONPath);
+    if (!project.build.input.ios.plugins.firebaseios) {
+        project.build.input.ios.plugins.firebaseios = {
+            path: 'plugins/iOS/firebaseios.zip',
+            active: true,
+            crashlytics: true
+        };
+    }
+    if (!project.build.input.android.plugins.modules || !project.build.input.android.plugins.modules.firebaseandroid) {
+        project.build.input.android.plugins.modules = project.build.input.android.plugins.modules
+            ? project.build.input.android.plugins.modules
+            : {};
+        project.build.input.android.plugins.modules.firebaseplugin = {
+            path: 'plugins/Android/firebaseplugin',
+            active: true
+        };
+    }
 
-  const output = JSON.stringify(project, null, '\t');
-  fs.writeFileSync(projectJSONPath, output, "utf8");
+    const output = JSON.stringify(project, null, '\t');
+    fs.writeFileSync(projectJSONPath, output, 'utf8');
 }
 
 function deleteRemainders() {
-  const nativePath = path.normalize(path.join(__dirname, "Native"));
-  rimraf.sync(nativePath, { recursive: true, force: true, disableGlob: true });
+    const nativePath = path.normalize(path.join(__dirname, 'Native'));
+    rimraf.sync(nativePath, { recursive: true, force: true, disableGlob: true });
 }
 
 Promise.all([getAndroidFirebasePlugin(), getIOSFirebasePlugin()])
-  .then(() => {
-    addDefaultConfigToProjectJSON();
-    deleteRemainders();
-  })
-  .catch((err) => {
-    console.error("An error occurred : ", err);
-    process.exit(1);
-  });
-
+    .then(() => {
+        addDefaultConfigToProjectJSON();
+        deleteRemainders();
+    })
+    .catch((err) => {
+        console.error('An error occurred : ', err);
+        process.exit(1);
+    });
